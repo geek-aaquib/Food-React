270f3bedaa4f4ae0c03dc2185b4390cb306a9f4c
geek-aaquib <mdaaquib.aalam143@gmail.com>
2024-05-07 14:40:30 -0400

    basic transition from html to javascript to react

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("div", {id: "Parent"}, 
+    ["I am Parent", React.createElement("div", {id: "children"}, 
+    [React.createElement("h2", {id: "child1"},"I am h2"), React.createElement("h3", {id: "child2"},"I am h3")]
+)]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
