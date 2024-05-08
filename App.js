d4587d13b6f03863c16936ed276c4e40706a3836
geek-aaquib <mdaaquib.aalam143@gmail.com>
2024-05-08 00:02:59 -0400

    igniting app

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", {id: "Parent"}, 
     ["I am Parent", React.createElement("div", {id: "children"}, 
     [React.createElement("h2", {id: "child1"},"I am h2"), React.createElement("h3", {id: "child2"},"I am h3")]
