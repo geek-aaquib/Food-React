4ba90ab80fbb0fdf58c69f45b2ddde43d9bebfec
geek-aaquib <mdaaquib.aalam143@gmail.com>
2024-05-08 21:00:21 -0400

    laying foundation

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
+// using core React
 const heading = React.createElement("div", {id: "Parent"}, 
     ["I am Parent", React.createElement("div", {id: "children"}, 
     [React.createElement("h2", {id: "child1"},"I am h2"), React.createElement("h3", {id: "child2"},"I am h3")]
 )]
 );
+
+//  using JSX
+const heading2 = (<h1 className="Par">This is created using JSX</h1>);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+const HeadingComponent = () => <h1>This is Functional Component</h1>
+
+root.render([heading, heading2, <HeadingComponent />]);
